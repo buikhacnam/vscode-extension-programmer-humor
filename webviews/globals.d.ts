6b0055ade8cc569919c1b2d79c41b553c7966e12
Casey Bui <buikhacnam11@gmail.com>
2022-09-18 08:40:06 +0700

    vscode notification

--- webviews/globals.d.ts
+++ webviews/globals.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="svelte" />
+import * as _vscode from 'vscode'
+
+declare global {
+	const tsvscode: {
+		postMessage: ({ type: string, value: any }) => void
+		getState: () => any
+		setState: (state: any) => void
+	}
+	const apiBaseUrl: string
+}
